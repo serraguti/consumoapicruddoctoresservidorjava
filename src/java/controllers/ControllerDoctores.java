91012349055c585041d67e6d1920cc9df7b994bc
Serra <pacoserranox@gmail.com>
2021-04-20 16:49:03 +0200

    Merge origin/master

--- src/java/controllers/ControllerDoctores.java
+++ src/java/controllers/ControllerDoctores.java
@@ -43,4 +43,37 @@ public class ControllerDoctores {
                 = this.service.eliminarDoctor(iddoctor);
         return "<h1 style='color:red'>Doctor eliminado, Code (" + code + ")</h1>";
     }
+
+    //PARA MODIFICAR, VAMOS A CREAR UN METODO PARA BUSCAR AL DOCTOR
+    //Y DIBUJAR LAS CAJAS CON SUS DATOS.
+    //UNA VEZ QUE PULSE SOBRE EL BOTON DE MODIFICAR, PUES OTRO METODO
+    //PARA MODIFICAR
+    //TENEMOS 2 OPCIONES:
+    //1.- DIBUJAR EL FORMULARIO DINAMICAMENTE
+    //2.- ENVIAR EL DOCTOR COMO OBJETO Y DIBUJAR SUS DATOS EN LA PAGINA
+    public String getDoctor(String iddoctor) throws IOException {
+        Doctor doctor = this.service.findDoctor(iddoctor);
+        String html = "<input type='hidden' name='iddoctor' value='"
+                + doctor.getIdDoctor() + "'/>";
+        html += "<label>Apellido: </label>";
+        html += "<input type='text' name='apellido' value='"
+                + doctor.getApellido() + "'/><br/>";
+        html += "<label>Especialidad: </label>";
+        html += "<input type='text' name='especialidad' value='"
+                + doctor.getEspecialidad() + "'/><br/>";
+        html += "<label>Salario: </label>";
+        html += "<input type='text' name='salario' value='"
+                + doctor.getSalario() + "'/><br/>";
+        html += "<label>Hospital: </label>";
+        html += "<input type='text' name='idhospital' value='"
+                + doctor.getIdHospital() + "'/><br/>";
+        return html;
+    }
+
+    public String updateDoctor(int iddoctor, String apellido,
+            String especialidad, int salario, int idhospital) throws IOException {
+        int code = this.service.updateDoctor(iddoctor, apellido, especialidad,
+                salario, idhospital);
+        return "<h1 style='color:fuchsia'>Doctor actualizado (" + code + ")</h1>";
+    }
 }
---
+++ web/webmodificardoctor.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:useBean id="controller" class="controllers.ControllerDoctores"
+             scope="request"/>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1 style="color:blue">Modificar doctor</h1>
+    </body>
+</html>
